a5c3274083e0458102effd3a45f166702c196eae
KRISHNAKUMARPS2002 <krishnakumarkichu2002@gmail.com>
2025-03-21 15:57:06 +0530

    done

--- src/app.js
+++ src/app.js
@@ -8,14 +8,15 @@ const userRoutes = require("./routes/userRoutes");
 const customerRoutes = require("./routes/customerRoutes");
 
 const app = express();
-app.use(
-  cors({
-    origin: ["http://localhost:3000", "https://localhost:3000"],
-    credentials: true,
-    methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
-    allowedHeaders: ["Content-Type", "Authorization"],
-  })
-);
+// In your Express app.js file
+const corsOptions = {
+  origin: ["http://localhost:3000", "http://localhost:3001"],
+  methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+  credentials: true,
+};
+
+app.use(cors(corsOptions));
 app.use(express.json());
 
 // Log all requests
