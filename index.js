2da117dd6bdff905c8f12063d68ae8e89ae8bddc
KRISHNAKUMARPS2002 <krishnakumarkichu2002@gmail.com>
2025-03-24 14:14:53 +0530

    port changed

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const app = require("./src/app");
 const logger = require("./src/config/logger"); // Import Winston logger
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5001;
 
 app.listen(PORT, () => {
   logger.info(`🚀 Server running on http://localhost:${PORT}`);
